cdc3fe5341aa44e9b027fc8cc3c11e0005a0d526
lemofack <lemofack@3il.fr>
2025-05-06 10:28:36 +0200

    travail d'aujourd'hui

--- Kabrel/Agence/admin.py
+++ Kabrel/Agence/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import register
 
 # Register your models here.
+@admin.register(register)
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'prename', 'email', 'password')
+    search_fields = ('name', 'prename', 'email')
+    list_filter = ('name', 'prename')
+    ordering = ('name',)
